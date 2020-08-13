a177223ab6f3fc1ff8481737ee7aba6ff25bf522
Scott Robinson <scott@quadhome.com>
2020-08-13 21:57:36 +1000

    Match the MANY-TO-MANY koan and solution aliases

--- src/koans/solutions/relationships.sql
+++ src/koans/solutions/relationships.sql
@@ -13,6 +13,6 @@ where b.id = 1
 -- Meditate on MANY-TO-MANY relationships
 select a.first_name, a.last_name, b.title
 from book b
-	join book_to_author_map map on b.id = map.book_id
-	join author a on map.author_id = a.id
+	join book_to_author_map btam on b.id = btam.book_id
+	join author a on btam.author_id = a.id
 where author_id in (1, 5, 6)
