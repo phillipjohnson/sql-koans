582f8ad8e971080cf50573e8ef341056eea7befc
TheUltimateNeverSettle <93392060+TheUltimateNeverSettle@users.noreply.github.com>
2022-02-01 10:26:03 -0500

    Updated

--- src/koans/basics.sql
+++ src/koans/basics.sql
@@ -2,22 +2,22 @@
 SELECT 1
 
 -- Meditate on lower-case queries
-_____ 1
+select 1
 
 -- Meditate on selecting all columns from a table
 select * from book
 
 -- Meditate on selecting one column (title) from a table
-select _____ from book
+select title from book
 
 -- Meditate on selecting some columns (title, available) from a table
-select _____, _____ from book
+select title, available from book
 
 -- Meditate on adding a new book
 insert into book(title, genre, year_published, available)
-values('_____', '_____', _____, 1)
+values('CSET2021', 'CSET', 2021, 1)
 
 -- Meditate on adding two new books
 insert into book(title, genre, year_published, available)
-values('_____', '_____', _____, 1)
-,('_____', '_____', _____, 1)
+values('CSET2022', 'CSET', 2022, 1)
+,('CSET2020', 'CSET', 2020, 1)
